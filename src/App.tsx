9921f6ee3716c626936a391f3173bdd3334d5654
filtermap <reducefiltermap@gmail.com>
2022-01-15 22:50:58 +0900

    use @emotion/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import React from "react";
-import { css, jsx } from "@emotion/core";
+import { css, jsx } from "@emotion/react";
 import * as Yomi from "./Yomi";
 import aboutDictionaryUrl from "./aboutDictionary.txt";
 
